df5af01c27af27f1a4be4ed735f249b4182d46d9
sudosubin <sudosubin@gmail.com>
2021-11-21 23:36:20 +0900

    feat: Change configuration js to ts

--- shopify.config.ts
+++ shopify.config.ts
@@ -1,6 +1,8 @@
+import {ShopifyConfig} from '@shopify/hydrogen/dist/esnext/types';
+
 export default {
   locale: 'en-us',
   storeDomain: 'hydrogen-preview.myshopify.com',
   storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
   graphqlApiVersion: 'unstable',
-};
+} as ShopifyConfig;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'vite';
+import {defineConfig, Plugin} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
 
 import shopifyConfig from './shopify.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [hydrogen(shopifyConfig)],
+  plugins: [hydrogen(shopifyConfig, {}) as Plugin[]],
   optimizeDeps: {include: ['@headlessui/react']},
 });
